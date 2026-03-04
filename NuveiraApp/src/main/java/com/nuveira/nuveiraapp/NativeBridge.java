0398b135ef1f1940433464f62be4a20a6ee79754
alda3sokaa <hassanibrahimo591@gmail.com>
2026-03-04 20:33:17 +0300

    Added NativeBridge.java

--- NuveiraApp/src/main/java/com/nuveira/nuveiraapp/NativeBridge.java
+++ NuveiraApp/src/main/java/com/nuveira/nuveiraapp/NativeBridge.java
@@ -1,13 +1,24 @@
 package com.nuveira.nuveiraapp;
 
-public class NativeBridge {
+import java.io.File;
 
+public class NativeBridge {
     static {
-        System.loadLibrary("native");
+        try {
+            File lib = new File("NativeBridge.dll").getAbsoluteFile();
+            System.load(lib.getAbsolutePath());
+            System.out.println("[SUCCESS] DLL loaded from: " + lib.getAbsolutePath());
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("[ERROR] Could not find or load NativeBridge.dll at: " + new File("NativeBridge.dll").getAbsolutePath());
+        }
     }
+
     public native void setOilData(float pricePerKg, float grams);
-    public native float getPricePerGramTL();
     public native float get50MLPrice();
-    public native float roundToNearest100(float value);
     public native float getUsdRate();
+    public native float getLiveRateFromC();
+
+    // ADDED THESE TWO - They were missing!
+    public native float getPricePerGramTL();
+    public native float roundToNearest100(float value);
 }
