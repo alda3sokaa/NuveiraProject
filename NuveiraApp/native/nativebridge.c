0a0ea54ca5558993d1bb3794ae46cdcf2fef9f5c
alda3sokaa <hassanibrahimo591@gmail.com>
2026-03-05 14:33:55 +0300

    API Implemented

--- NuveiraApp/native/nativebridge.c
+++ NuveiraApp/native/nativebridge.c
@@ -4,8 +4,9 @@
 #include <string.h>
 #include "com_nuveira_nuveiraapp_NativeBridge.h"
 
-
 float USD_TO_TL = 44.10f; 
+float globalPricePerKg = 0.0f;
+float globalGrams = 0.0f;
 
 const float SUEDE_BAG_COST = 26.5f;
 const float CARTON_BAG_COST = 11.3f;
@@ -17,67 +18,51 @@ const float DEVELOPMENT_COST = 15.0f;
 const float SAMPLE_COST = 5.0f;
 const float THANK_YOU_CARD = 4.0f;
 const float RAMADAN_CARD = 5.0f;
-
 const float OIL_IN_50ML = 16.5f;
-
 const float MARKETING_PERCENTAGE = 1.1f;
 const float FAULT_PERCENTAGE = 1.03f;
 const float PROFIT_PERCENTAGE = 2.2f;
-
 const float SMALL_BATCH_TAX = 20.0f;
 const float MEDIUM_BATCH_TAX = 10.0f;
 const float LARGE_BATCH_TAX = 5.0f;
 
-// Global state to store input from Java
-float globalPricePerKg = 0.0f;
-float globalGrams = 0.0f;
+JNIEXPORT void JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_setExchangeRate(JNIEnv *env, jobject obj, jfloat rate) {
+    USD_TO_TL = rate;
+}
 
-JNIEXPORT void JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_setOilData
-  (JNIEnv *env, jobject obj, jfloat pricePerKg, jfloat grams) {
+JNIEXPORT void JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_setOilData(JNIEnv *env, jobject obj, jfloat pricePerKg, jfloat grams) {
     globalPricePerKg = pricePerKg;
     globalGrams = grams;
 }
 
-JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_getPricePerGramTL
-  (JNIEnv *env, jobject obj) {
+JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_getPricePerGramTL(JNIEnv *env, jobject obj) {
     float pricePerKg = globalPricePerKg;
-
     if (globalGrams < 100.0f) pricePerKg += SMALL_BATCH_TAX;
     else if (globalGrams < 250.0f) pricePerKg += MEDIUM_BATCH_TAX;
     else if (globalGrams < 500.0f) pricePerKg += LARGE_BATCH_TAX;
-
-    float pricePerGramUsd = pricePerKg / 1000.0f;
-    return pricePerGramUsd * USD_TO_TL;
+    return (pricePerKg / 1000.0f) * USD_TO_TL;
 }
 
-JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_get50MLPrice
-  (JNIEnv *env, jobject obj) {
+JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_get50MLPrice(JNIEnv *env, jobject obj) {
     float gramPriceTl = Java_com_nuveira_nuveiraapp_NativeBridge_getPricePerGramTL(env, obj);
     float oilAmountPriceTl = OIL_IN_50ML * gramPriceTl;
-
-
     float retailPriceInTl = (oilAmountPriceTl + CARTON_BAG_COST + SUEDE_BAG_COST +
                 ETHANOL_50ML_COST + BOTTLE_COST_50ML + 
                 STICKER_COST + LABOR_COST + DEVELOPMENT_COST + 
                 SAMPLE_COST + RAMADAN_CARD + 
                 THANK_YOU_CARD) * MARKETING_PERCENTAGE
                 * FAULT_PERCENTAGE * PROFIT_PERCENTAGE;
-    
     return retailPriceInTl;
 }
 
-JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_roundToNearest100
-  (JNIEnv *env, jobject obj, jfloat price) {
-
+JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_roundToNearest100(JNIEnv *env, jobject obj, jfloat price) {
     return roundf(price / 100.0f) * 100.0f;
 }
 
-JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_getLiveRateFromC
-  (JNIEnv *env, jobject obj) {
+JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_getLiveRateFromC(JNIEnv *env, jobject obj) {
     return USD_TO_TL;
 }
 
-JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_getUsdRate
-  (JNIEnv *env, jobject obj) {
+JNIEXPORT jfloat JNICALL Java_com_nuveira_nuveiraapp_NativeBridge_getUsdRate(JNIEnv *env, jobject obj) {
     return USD_TO_TL;
 }
